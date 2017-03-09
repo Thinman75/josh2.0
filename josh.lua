0daa45becd86f12d23b7eb868a1911f5fb166e0d
nesven <kol@mithos.localdomain>
2017-03-08 19:17:01 -0800

    added bot

---
+++ josh.lua
@@ -0,0 +1,24 @@
+local discordia = require('discordia')
+local client = discordia.Client()
+local token = args[2]
+
+local mema = io.open('Mema.txt', "r");
+local arr = {}
+for line in mema:lines() do
+	table.insert (arr, line);
+end
+
+client:on('ready', function()
+	print('Logged in as '.. client.user.username)
+end)
+
+client:on('messageCreate', function(message)
+	if message.content == '!dankmema' then
+		message.channel:sendMessage('Your legacy dose of dank mema: \n'.. arr[math.random(439)])
+	elseif message.content == "!f" then
+		message.channel:sendMessage(message.author.username.. ' has paid their respects')
+	end
+end)
+
+client:run(token)
+
